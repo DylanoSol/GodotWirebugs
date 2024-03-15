1c194c7902f421cfcbcfda36e94e6345e0d293d8
DylanoSol <dylanosolter@hotmail.com>
2024-03-15 14:37:11 +0100

    [ADDED] Basic camera

--- Project/WireBugs/Player/PlayerCharacter.gd
+++ Project/WireBugs/Player/PlayerCharacter.gd
@@ -1,13 +1,28 @@
 extends CharacterBody3D
 
+@onready var camerafocus = $camerafocus
+
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@export var camera_speed_x = 0.5
+@export var camera_speed_y = 0.5
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
+func _ready(): 
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _input(event): 
+	if event is InputEventMouseMotion:
+		# x axis mouse movement
+		rotate_y(-deg_to_rad(event.relative.x) * camera_speed_x)
+		# y axis mouse movement. Do this on the camera focus
+		camerafocus.rotate_x(-deg_to_rad(event.relative.y) * camera_speed_y)
+		
+	
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -19,7 +34,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
