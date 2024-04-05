a24d774ccf77799ec0c649575de2f9dd7d5b427a
DylanoSol <dylanosolter@hotmail.com>
2024-04-05 19:18:10 +0200

    [ADDED] Script for hud element

---
+++ Project/WireBugs/UI/ReadySymbol.gd
@@ -0,0 +1,21 @@
+extends Node
+
+@export var isOnCooldown = true; 
+var baseCooldown = 10; 
+var cooldown = baseCooldown; 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (isOnCooldown) : 
+		self.visible = false; 
+		cooldown -= delta; 
+		if (cooldown < 0) : 
+			isOnCooldown = false; 
+			cooldown = baseCooldown; 	
+	else : 
+		self.visible = true; 
